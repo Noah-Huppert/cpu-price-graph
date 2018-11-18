b293e3028dda17377ecc616f1f000c8925ab66d7
Noah Huppert <contact@noahh.io>
2018-11-18 13:04:46 -0500

    found avrg of binned ratings

--- graph.py
+++ graph.py
@@ -5,30 +5,52 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 # Data
-x = []
-raw_data = []
+# ... Raw data
+prices = []
+ratings = []
+price_rating = []
 
 with open('specs.json', 'r') as f:
     for i in json.load(f):
         if i['price'] < 0:
             continue
 
-        x.append(i['price'])
-        raw_data.append([i['price'], i['rating']])
+        prices.append(i['price'])
+        ratings.append(i['rating'])
+        price_rating.append([i['price'], i['rating']])
 
-x = np.array(x)
-
+# ... Binned
 price_bins = np.array([float(i*100) for i in range(40)])
-bin_indexes = np.digitize(x, price_bins)
+price_bin_indexes = np.digitize(np.array(prices), price_bins)
 
-bined_data_x = []
-bined_data_y = []
-for i, bin_i in enumerate(bin_indexes):
-    bined_data_x.append(price_bins[bin_i-1])
-    bined_data_y.append(raw_data[i][1])
+binned_prices = []
+binned_ratings = []
+binned_price_rating = []
 
-z = np.polyfit(bined_data_x, bined_data_y, 1)
-p = np.poly1d(z)
+binned_summed_ratings = []
+binned_summed_ratings_count = []
+for i in range(len(price_bins)):
+    binned_summed_ratings.append(0)
+    binned_summed_ratings_count.append(0)
+
+for i, bin_i in enumerate(price_bin_indexes):
+    price = price_bins[bin_i-1]
+    rating = price_rating[i][1]
+
+    binned_price_rating.append([price, rating])
+    binned_prices.append(price)
+    binned_ratings.append(rating)
+    binned_summed_ratings[bin_i-1] += rating
+    binned_summed_ratings_count[bin_i-1] += 1
+
+binned_avrg_ratings = []
+for i, summed_ratings in enumerate(binned_summed_ratings):
+    if binned_summed_ratings_count[i] == 0:
+        binned_avrg_ratings.append(0)
+    else:
+        binned_avrg_ratings.append(summed_ratings / binned_summed_ratings_count[i])
+
+print("Correlation: {}".format(np.correlate(prices, ratings)))
 
 # Plot
 fig, ax = plt.subplots()
@@ -36,9 +58,8 @@ fig, ax = plt.subplots()
 ax.set_xlabel("Price")
 ax.set_ylabel("Rating")
 
-ax.scatter(bined_data_x, bined_data_y, label='Price vs Rating')
-
-ax.plot(bined_data_x, p(bined_data_x), label='Fit', color='orange')
+ax.scatter(binned_prices, binned_ratings, label='Binned Price vs Rating', color='blue')
+ax.plot(price_bins, binned_avrg_ratings, label='Average Binned Price vs Rating', color='red')
 
 fig.legend()
 plt.show()
